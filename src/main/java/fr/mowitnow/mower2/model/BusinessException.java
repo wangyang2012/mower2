733e1d844b142e81596bcf57aeda90c1b7d6ccd1
YWANG <wangyang1712@gmail.com>
2018-03-19 17:38:55 +0100

    Add file reader

--- src/main/java/fr/mowitnow/mower2/model/BusinessException.java
+++ src/main/java/fr/mowitnow/mower2/model/BusinessException.java
@@ -1,4 +1,11 @@
 package fr.mowitnow.mower2.model;
 
 public class BusinessException extends Exception {
+    public BusinessException(){
+        super();
+    }
+
+    public BusinessException(String message){
+        super(message);
+    }
 }
--- src/main/java/fr/mowitnow/mower2/util/FileUtil.java
+++ src/main/java/fr/mowitnow/mower2/util/FileUtil.java
@@ -1,4 +1,25 @@
 package fr.mowitnow.mower2.util;
 
+import fr.mowitnow.mower2.model.BusinessException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class FileUtil {
+    public static String readFile(String fileName) throws BusinessException {
+        //read file into stream, try-with-resources
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+            StringBuilder sb = new StringBuilder();
+            for(String line : lines){
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            throw new BusinessException("Impossible de lire le fichier d'entrée");
+        }
+    }
 }
